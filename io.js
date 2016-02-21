294e1da3cb87ff78a716d496c32c90de872c23cb
Ben W <reccanti@gmail.com>
2016-02-21 02:08:02 -0500

    Fancy animation
    Added a fancy animation where the text rolls up instead of just disappearing

--- io.js
+++ io.js
@@ -1,13 +1,28 @@
 var socketio = require('socket.io');
 var io = socketio();
 
+var count = 0;
+
+
+/**
+ * This function handles an increment event on the socket
+ */
+function onIncrement(socket) {
+    socket.on("incrementCount", function(data) {
+        count++;
+        socket.emit("updatePara", { val: count });
+    });
+}
+
+
 /**
  * This function handles the initialization of the socket.io connection
  */
 io.sockets.on("connection", function(socket) {
     //console.log(socket);
     socket.join("room1");
-    io.sockets.in("room1").emit("updatePara", { data:"butts" });
+    onIncrement(socket);
+    
 });
 
 module.exports = io;
--- public/javascripts/socketHandler.js
+++ public/javascripts/socketHandler.js
@@ -1,8 +1,50 @@
 var socket;
+var countField;
+
 window.addEventListener("load", function() {
+    countField = document.getElementById("count");
     socket = io.connect(); 
     socket.on("updatePara", function(data) {
-        console.log(data);
+        var node = createNode(data.val);
+        addNode(node);
+        setTimeout(function() {
+            removeNode(node);
+        }, 1500);
     });
-    
 });
+
+
+setInterval(incrementCount, 3000);
+
+    
+/**
+ * A function that tells the server to increment its count
+ */
+function incrementCount() {
+    socket.emit("incrementCount", {});
+}
+
+
+/**
+ * Create a div node containing a value
+ */
+function createNode(val) {
+    var divNode = document.createElement("div");
+    divNode.innerHTML = val;
+    return divNode;
+}
+
+
+/**
+ * Add a node to the count div
+ */
+function addNode(node) {
+    countField.appendChild(node);
+}
+
+/**
+ * Remove a div node from the count div
+ */
+function removeNode(node) {
+    countField.removeChild(node);
+}
